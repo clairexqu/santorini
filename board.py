0e0646df63e9fc147e96a40c877c7fc97eb528a5
AnotherGapYear <luis.mendozaperez@yale.edu>
2023-12-04 21:24:53 -0500

    cli updates and board

--- board.py
+++ board.py
@@ -10,3 +10,24 @@ class Board:
                 print(f"|{str(cell):^2}", end="")
             print("|")
         print("+--+--+--+--+--+")
+
+class Board:
+    """Class representing the Santorini game board"""
+
+    def __init__(self):
+        self._board_state = [
+            [0, 0, 0, 0, 0],
+            [0, "0Y", 0, "0B", 0],
+            [0, 0, 0, 0, 0],
+            [0, "0A", 0, "0Z", 0],
+            [0, 0, 0, 0, 0]
+        ]
+
+    def display(self):
+        """Display the current board state."""
+        for row in self._board_state:
+            print("+--+--+--+--+--+")
+            for cell in row:
+                print(f"|{cell}", end=" ")
+            print("|")
+        print("+--+--+--+--+--+")
--- cli.py
+++ cli.py
@@ -1,40 +1,41 @@
 import sys
 
 from game import Game
+from board import Board
 
 class SantoriniCLI:
     """Driver class for a command-line REPL interface to the Santorini game"""
 
     def __init__(self):
         self._game = Game()
-
-        self._choices = {
-            "1": self._start_game,
-            "2": self._quit,
-        }
-
-    def _display_menu(self):
-        print("""--------------------------------
-Enter command
-1: Start game
-2: Quit""")
+        self._board = Board()
 
     def run(self):
-        """Display the menu and respond to choices."""
+        """Start the game with the provided board state."""
 
-        while True:
-            self._display_menu()
-            choice = input(">")
-            action = self._choices.get(choice)
+        # Display the initial board state
+        print(self._board)
 
-            if action:
-                action()
-            else:
-                print("{0} is not a valid choice".format(choice))
+        # Perform the first move
+        self._perform_move()
 
-    def _start_game(self):
-        print("Starting Santorini game...")
-        self._game.start()
+    def _perform_move(self):
+        """Perform a move in the game."""
+
+        # Get input for worker and direction to move
+        worker = input("Select a worker to move\n")
+        direction_to_move = input("Select a direction to move (n, ne, e, se, s, sw, w, nw)\n")
+        direction_to_build = input("Select a direction to build (n, ne, e, se, s, sw, w, nw)\n")
+
+        # Perform the move
+        try:
+            self._game.perform_move(worker, direction_to_move, direction_to_build)
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            sys.exit(1)
+
+        # Display the updated board state
+        self._board.display()
 
     def _quit(self):
         sys.exit(0)
@@ -44,5 +45,5 @@ if __name__ == "__main__":
     try:
         SantoriniCLI().run()
     except Exception as e:
-        print("Sorry! Something unexpected happened. Check the logs or contact the developer for assistance.")
+        print("Sorry! Something unexpected happened.")
         sys.exit(0)
