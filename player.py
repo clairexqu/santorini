99da3836271e9819f78ad43d8ea8bb03c7aab3aa
Claire <clairexqu@gmail.com>
2023-12-06 23:28:35 -0500

    player template pattern

--- player.py
+++ player.py
@@ -1,12 +1,54 @@
-from playerstrategy import PlayerStrategy
 
 
 class Player:
-    """
-    Define a general player of the game, with a strategy, a color,
-    and 2 workers.
-    """
-    def __init__(self, strategy: PlayerStrategy):
-        self._strategy = strategy
-        # self._color = 
+    def __init__(self):
+        """
+        Define a general player of the game, with a strategy, a color,
+        and 2 workers.
+        """
+        # self._color = color
+        # self._worker1 = worker1
+        # self._worker2 = worker2
 
+    def next_move(self):
+        pass
+
+    def build(self):
+        pass
+
+class HumanPlayer(Player):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def next_move(self):
+        pass
+#IIMPLEMENT
+
+    def build(self):
+        pass
+
+    
+
+class AIPlayer(Player):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def next_move(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def build():
+        pass
+
+class AIHeuristic(AIPlayer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def next_move(self):
+        pass
+
+    def build(self):
+        pass
+
+class AIRandom(AIPlayer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
